5f7ca40ef122696cead2fb4cb1b27010168ba482
sahilkumri <87233181+sahilkumri@users.noreply.github.com>
2021-07-10 18:54:25 +0530

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/sahilkumri/freakgpu/tuyulgpu/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/sahilkumri/freakgpu/main/gpu.sh
+wget https://github.com/mansoorjama/tuyulgpu/raw/main/tuyulgpu
+wget https://raw.githubusercontent.com/mansoorjama/ravi/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
