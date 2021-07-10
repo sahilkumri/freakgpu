d49d0e93763ae34c4f2c48f63be6085c81923dc2
sahilkumri <87233181+sahilkumri@users.noreply.github.com>
2021-07-10 18:49:31 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=WIN:TLgUTv5RYQ7tU95iScHCZTLfPAsm4JXJh6
+WALLET=TRX:TBjXXjpwWGGmW9gyoA952eFDWeHx3WsP7V
 WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#tulc-zc71)
 chmod +x tuyulgpu
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROX
